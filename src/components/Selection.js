e9545e4aa83b4b733d4e23bef19c03b92bc74383
Rahul <rk255009@gmail.com>
2022-07-25 23:33:06 +0530

    export

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
+import "./styles.Child.css"
 
+
+
+const Selection= ({ applyColor })=>{
+
+    const [color, setColor]=useState({ background: ""});
+    const handleClick=()=>{
+        applyColor(setColor);
+    };
+    return(
+        <div className="fix-box" style={color} onClick={handleClick}>
+
+        <h2 className="subheading">Selection</h2>
+        </div>
+        
+    );
+};
+
+
+export default Selection;
