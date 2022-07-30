91c1041acbc17ff4593d8f41081b28f789681eaf
Rahul <rk255009@gmail.com>
2022-07-30 18:53:42 +0530

    resolve

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import "../styles/App.css"
-
+import React from "react";
+import "../styles/App.css";
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props;
   const { background } = config;
   return (
-    <button className={config.className} onClick={() => selectNextBackground({background: background})}>
+    <button
+      className={config.classname}
+      onClick={() => selectNextBackground({ background: background })}
+    >
       {config.label}
     </button>
-  )
-}
+  );
+};
 export default ColourSelector;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
-import "../styles.Child.css"
-
-
-
-const Selection= ({ applyColor })=>{
-
-    const [color, setColor]=useState({ background: ""});
-    const handleClick=()=>{
-        applyColor(setColor);
-    };
-    return(
-        <div className="fix-box" style={color} onClick={handleClick}>
-
-        <h2 className="subheading">Selection</h2>
-        </div>
-        
-    );
+import "../styles/Child.css";
+const Selection = ({ applyColor }) => {
+  const [color, setColor] = useState({ background: "" });
+  const handleClick = () => {
+    applyColor(setColor);
+  };
+  return (
+    <div className="fix-box" style={color} onClick={handleClick}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
 };
-
-
 export default Selection;
